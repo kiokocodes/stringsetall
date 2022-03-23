59415d95810227f1486808575b07619c16d86f8c
Joseph Kioko <kiokocodes@gmail.com>
2022-03-23 03:10:33 +0300

    made let and const decalaration to varaibles.js file

--- variables.js
+++ variables.js
@@ -1,3 +1,20 @@
-console.log("Hallo Wlt!");
-console.error("Diese ist eine Fehler");
-console.warn("Diese ist ien Wernung!!");
+// For the most part you don't want to use var because of scope conflicts
+//let and const have a lot of functionality - let: reassigns, const cannot.
+//Always use const unless you are going to reassign a value
+
+//Using let to declare a variable
+
+/* let meinName = "Kioko";
+meinName = "Kiamba";
+
+console.log(meinName);
+*/
+
+
+// Using const to store a value
+
+const meineLieben = "Adeleide und Milan";
+
+meineLieben = "was ist das?";
+
+console.log(meineLieben);
